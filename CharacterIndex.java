26118a823505d8140ccca19eba57335a97e86f5d
Aravindh55 <91793881+Aravindh55@users.noreply.github.com>
2021-10-16 22:30:17 +0530

    Add files via upload

---
+++ CharacterIndex.java
@@ -0,0 +1,13 @@
+package week3.day1;
+
+public class CharacterIndex {
+
+	public static void main(String[] args) {
+		String text = "Java Exercise";
+		int index1 = text.indexOf('E');
+		int index2 = text.indexOf('s');
+		System.out.println("the index of E is : "+index1);
+		System.out.println("the index of s is : "+index2);
+	}
+
+}
