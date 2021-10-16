2f1ea270b0e9a5e018cc8a7446ad28ca5473f64d
Aravindh55 <91793881+Aravindh55@users.noreply.github.com>
2021-10-16 21:13:40 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,17 @@
+package org.system;
+
+public class Desktop extends Computer {
+	public void desktopSize() {
+		System.out.println("14 inch");
+
+	}
+	public static void main(String[] args) {
+		Desktop a=new Desktop();
+		a.computerModel();
+		a.desktopSize();
+		
+		
+	}
+
+}
+
